d3027eaf137a73a3bb7a9a9a86a002875b79fae3
Anton <polyncev1991@mail.ru>
2022-08-06 12:39:15 +0300

    Создал программу на max and min 3 чисел

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 7;
+int b = 3;
+int c = 2;
+
+int max = a;
+
+if (b > a ) max = b;
+if (c > max ) max = c;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
